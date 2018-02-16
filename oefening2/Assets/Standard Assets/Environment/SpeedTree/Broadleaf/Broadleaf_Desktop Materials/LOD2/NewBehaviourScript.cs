be3ac9bd733d4f1f393351d4b8917b3a27d733dc
Pieter <pieterman1995@hotmail.com>
2018-02-16 12:29:13 +0100

    dinge
    dinge

---
+++ oefening2/Assets/Standard Assets/Environment/SpeedTree/Broadleaf/Broadleaf_Desktop Materials/LOD2/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public Color objectcolour = new Color();
+
+	public Renderer objectRenderer;
+	// Use this for initialization
+	void Start () {
+		objectRenderer.material.color = objectcolour;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x,Mathf.PingPong(Time.time, 3), transform.position.z); 
+	}
+}
